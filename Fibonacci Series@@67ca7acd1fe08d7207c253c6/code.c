ab5324582022efacee7e6b090e0154999bb34e96
Saurya Thakur <shauryathakur89180@gmail.com>
2025-04-07 20:39:38 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -13,3 +13,18 @@ void fibonacciSeries(int n) {
     }
     printf("\n");
 }
+
+int main() {
+    int terms;
+    
+    printf("Enter the number of terms: ");
+    scanf("%d", &terms);
+    
+    if (terms < 2) {
+        printf("Please enter a number greater than or equal to 2.\n");
+    } else {
+        fibonacci(terms);
+    }
+    
+    return 0;
+}
